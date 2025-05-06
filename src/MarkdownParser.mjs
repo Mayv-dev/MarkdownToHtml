f7ffe99e24683153934d52b4089d739de1ba50da
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2025-05-06 20:24:33 +0100

    Passing h1 test 1
    Tetsing for string with single header 1, no content.
    - Test "#".

---
+++ src/MarkdownParser.mjs
@@ -0,0 +1,27 @@
+import { newToken, Token, token } from "./data/Token.mjs";
+import { TokenType } from "./data/TokenType.mjs";
+
+/**
+ * Parses a string of Markdown into a list of tokens
+ * @param {string} inputStr
+ * @returns {[Token]}
+ */
+export const parseMarkdown = (inputStr) => {
+  const blocks = inputStr.split("\n");
+  let tokens = [];
+  blocks.forEach((block) => {
+    tokens = [...tokens, findBlockType(block)];
+  });
+  return tokens;
+};
+
+/**
+ * Finds the type of the Markdown block
+ * @param {string} block
+ * @returns {Token}
+ */
+const findBlockType = (block) => {
+  if (block.startsWith("#") && block.length === 1) {
+    return newToken(TokenType.h1, "");
+  }
+};
